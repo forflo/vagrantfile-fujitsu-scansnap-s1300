16adf9e272317f6d15cbfc260aaad10b37ca963c
Florian Mayer <mayer.florian@web.de>
2020-03-29 03:10:16 +0200

    Update convert_en_bulk.sh

--- convert_en_bulk.sh
+++ convert_en_bulk.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-# dirty in-situ ad-hoc processing
+# Dirty in-situ ad-hoc processing
+# To be called from inside of this VM!
 find /vagrant/scanout_raw -type f -name "*.png" \
     | parallel -j2 tesseract -l eng+deu {} {.} pdf
